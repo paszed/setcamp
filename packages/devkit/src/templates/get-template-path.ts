7f78184b20c16b5f88313326647aa5f86ceac9a9
paszed <edvard.pasz@icloud.com>
2026-07-15 09:43:30 +0200

    refactor: complete NodeNext migration

--- packages/devkit/src/templates/get-template-path.ts
+++ packages/devkit/src/templates/get-template-path.ts
@@ -1,9 +1,14 @@
 import { join } from "node:path";
 
 import { findSetcampRoot } from "../project/index.js";
+
 /**
  * Returns the absolute path to a template.
  */
 export function getTemplatePath(name: string): string {
-	return join(findSetcampRoot(), "templates", name);
+	const path = join(findSetcampRoot(), "templates", name);
+
+	console.log("Template path:", path);
+
+	return path;
 }
